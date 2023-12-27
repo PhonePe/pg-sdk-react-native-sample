12e109bdcf0317ef61561e6981bb5407e7dfa08b
Anshuman Singh <anshuman.singh1@phonepe.com>
2023-12-27 17:10:36 +0530

    Added package v2.0.0 support

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -8,64 +8,27 @@ import PPTextField from "./Container/PPTextField";
 import PPHTextField from "./Container/PPHStack";
 
 const Home = () => {
-
   const [requestBody, setRequestBody] = useState<string>('');
   const [merchantId, setMerchantId] = useState<string>('');
   const [appId, setAppId] = useState<string>('');
 
   const [checksum, setChecksum] = useState<string>('');
 
-  const [open, setOpen] = useState(false);
-  const [dropDownValue, setValue] = useState('');
-
-  const [items, setItems] = useState([
-    { label: 'Debit', value: '/v4/debit' },
-    { label: 'B2B PG', value: '/pg/v1/pay' }
-  ]);
-
-
   const [openEnvironment, setOpenEnvironment] = useState(false);
   const [environmentDropDownValue, setEnvironmentValue] = useState('PRODUCTION');
 
   const [environements, setEnvironment] = useState([
-    { label: 'UAT', value: 'UAT' },
-    { label: 'UAT_SIM', value: 'UAT_SIM' },
+    { label: 'SANDBOX', value: 'SANDBOX' },
     { label: 'PRODUCTION', value: 'PRODUCTION' }
   ]);
 
-  const [headers, setHeaders] = useState<any>({});
-  const [headerKey, setHeaderKey] = useState<string>('');
-  const [headerValue, setHeaderValue] = useState<string>('');
-
   const [packageName, setPackageName] = useState<string>('');
-  const [callbackURL, setCallbackURL] = useState<string>('');
-
-  const handleAddHeader = () => {
-    if (!headerKey || !headerValue)
-      return
-
-    setHeaders({
-      ...headers,
-      [headerKey]: headerValue
-    })
-    setHeaderKey("");
-    setHeaderValue("");
-  };
-
-  const handleResetHeader = () => {
-    setHeaders({})
-    setHeaderKey("");
-    setHeaderValue("");
-  };
-
-
+  const [callbackURL, setCallbackURL] = useState<string>('reactDemoAppScheme');
+ 
   const handleStartTransaction = () => {
-    if (dropDownValue == "/pg/v1/pay") {
-      PhonePePaymentSDK.startPGTransaction(
+      PhonePePaymentSDK.startTransaction(
         requestBody,
         checksum,
-        dropDownValue,
-        headers,
         packageName,
         callbackURL
       ).then(a => {
@@ -73,19 +36,6 @@ const Home = () => {
       }).catch(error => {
         setMessage("error:" + error.message);
       })
-    } else {
-      PhonePePaymentSDK.startContainerTransaction(
-        requestBody,
-        checksum,
-        dropDownValue,
-        headers,
-        callbackURL
-      ).then(a => {
-        setMessage(JSON.stringify(a));
-      }).catch(error => {
-        setMessage("error:" + error.message);
-      })
-    }
   };
 
   const initPhonePeSDK = () => {
@@ -170,7 +120,7 @@ const Home = () => {
     <SafeAreaView>
       <View style={style.container}>
         <ScrollView>
-          <Text style={style.heading}>React Native Demo App</Text>
+          <Text style={style.heading}>RN Merchant Demo App</Text>
 
           <PPTextField
             title="Merchant ID:"
@@ -187,6 +137,7 @@ const Home = () => {
           <Text>Environment:</Text>
           <DropDownPicker
             open={openEnvironment}
+            listMode="SCROLLVIEW"
             value={environmentDropDownValue}
             items={environements}
             setOpen={setOpenEnvironment}
@@ -212,57 +163,6 @@ const Home = () => {
             setValue={setChecksum}
           />
 
-          <Text>API Endpoint:</Text>
-          <DropDownPicker
-            open={open}
-            value={dropDownValue}
-            items={items}
-            setOpen={setOpen}
-            setValue={setValue}
-            setItems={setItems}
-            style={style.dropdown}
-          />
-
-          {Platform.OS == "ios" &&
-            <View>
-              <Text>Headers:</Text>
-              <View>
-                {
-                  Object.keys(headers)?.map(key => {
-                    (
-                      <Text>
-                        {key} : {headers[key]}
-                      </Text>
-                    )
-                  })
-                }
-              </View>
-              <PPHTextField
-                title="Key"
-                placeholder="Enter Header Key"
-                setValue={setHeaderKey}
-              />
-              <PPHTextField
-                title="Value"
-                placeholder="Enter Header Value"
-                setValue={setHeaderValue}
-              />
-
-              <Text style={{ marginVertical: 4 }}>{JSON.stringify(headers)}</Text>
-              <View style={style.bckView}>
-                <PPButton
-                  headerStyle={style.centerLabel}
-                  title="Reset Header"
-                  onPress={handleResetHeader}
-                />
-                <PPButton
-                  headerStyle={style.centerLabel}
-                  title="Add Header"
-                  onPress={handleAddHeader}
-                />
-              </View>
-            </View>
-          }
           {Platform.OS == "android" &&
             <PPTextField
               title="Package Name: "
@@ -273,8 +173,9 @@ const Home = () => {
           {Platform.OS == "ios" &&
 
             <PPTextField
-              title="CallbackURL:"
-              placeholder="Enter callback url"
+              title="App Schema:"
+              value={callbackURL}
+              placeholder="Enter app schema url"
               setValue={setCallbackURL}
             />
           }
@@ -294,7 +195,7 @@ const Home = () => {
               onPress={handleIsPaytmInstalled}
             />
             <PPButton
-              title="Check GPay"
+              title="Check  GPay"
               onPress={handleIsGPayAppInstalled}
             />
           </View>
